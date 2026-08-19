f0250bb260a35ae813431255943265a554c553e9
BenHex0 <194300233+BenHex0@users.noreply.github.com>
2026-08-19 18:40:39 +0200

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+   public:
+    bool dfs(int node, vector<vector<int>>& nodes, vector<int>& state, vector<int>& order) {
+        if (state[node] == 2) return true;
+        if (state[node] == 1) return false;
+
+        state[node] = 1;
+
+        for (int n : nodes[node]) {
+            if (!dfs(n, nodes, state, order)) return false;
+        }
+
+        state[node] = 2;
+        order.push_back(node);
+
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>> prerequisites) {
+        vector<vector<int>> nodes(numCourses);
+
+        for (auto& prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int prerequisiteCourse = prerequisite[1];
+            nodes[prerequisiteCourse].push_back(course);
+        }
+
+        vector<int> state(numCourses, 0);
+        vector<int> order;  // local -- not a parameter, not returned, just used internally
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                if (!dfs(i, nodes, state, order))
+                    return false;  // cycle -> no valid order, don't bother printing
+            }
+        }
+
+        reverse(order.begin(), order.end());
+
+        cout << "Topological order: ";
+        for (int x : order) cout << x << ' ';
+        cout << '\n';
+
+        return true;
+    }
+};
